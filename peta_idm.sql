ce98bed728edf508ffc24f4610811e449170a66e
MWahyuKandrival <76944693+MWahyuKandrival@users.noreply.github.com>
2022-03-30 02:15:19 +0700

    Update peta_idm.sql

--- peta_idm.sql
+++ peta_idm.sql
@@ -70,7 +70,7 @@ CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
