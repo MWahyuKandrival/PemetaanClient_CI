94ff8451d6eb8d8a8e85341d54bdd84af457950c
MWahyuKandrival <76944693+MWahyuKandrival@users.noreply.github.com>
2022-04-11 09:35:00 +0700

    Update peta_idm.sql

--- peta_idm.sql
+++ peta_idm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2022 at 08:00 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.6
+-- Generation Time: Apr 11, 2022 at 04:16 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,15 +32,28 @@ CREATE TABLE `client` (
   `nama_client` varchar(50) NOT NULL,
   `owner` varchar(50) NOT NULL,
   `alamat` varchar(50) NOT NULL,
+  `negara` varchar(255) NOT NULL,
+  `region` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `no_hp` varchar(15) NOT NULL,
   `domain` varchar(50) NOT NULL,
   `latitude` varchar(20) NOT NULL,
   `longitude` varchar(20) NOT NULL,
-  `mulai_kerja_sama` date NOT NULL,
-  `henti_kerja_sama` date NOT NULL,
-  `status_kerja_sama` varchar(20) NOT NULL
+  `mulai_kerja_sama` date DEFAULT NULL,
+  `henti_kerja_sama` date DEFAULT NULL,
+  `status_kerja_sama` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nama_client`, `owner`, `alamat`, `negara`, `region`, `email`, `no_hp`, `domain`, `latitude`, `longitude`, `mulai_kerja_sama`, `henti_kerja_sama`, `status_kerja_sama`) VALUES
+(943752156, 'Gedung Sate', 'BUMN', 'Jln. Diponegoro ', 'Indonesia', 'Jawa Barat', 'gedungsate@gmail.go.id', '0888888', 'gedungsate.com', '-6.90062282242416', '107.6197138895795', NULL, NULL, NULL),
+(943752157, 'Inovindo web', 'Novi ', 'Jl Batu sari', 'Indonesia', 'Jawa Barat', 'novipengusaha@gmail.com', '088888', 'inovindo.com', '-6.981648368742416', '107.67407244110632', NULL, NULL, NULL),
+(943752158, 'Politeknik Caltex Riau', 'Chevron', 'Jln. Umban sari', 'Indonesia', 'Riau', 'pcr@pcr.ac.id', '08888', 'pcr.ac.id', '0.5702455694186912', '101.42558992756356', NULL, NULL, NULL),
+(943752159, 'Petronas Twin tower', 'Petronas', 'Concourse level', 'Malaysia', 'Kuala Lumpur', 'twintower@gmail.com', '0888888', 'twintower.com', '3.158409817741703', '101.7117064263871', NULL, NULL, NULL),
+(943752160, 'Nanyang Politeknik', 'Nanyang', 'Downtown core district', 'Singapore', 'North East region', 'nanyang@nanyang.ac,sg', '09898989', 'nanyang.ac.sg', '1.3813196067814155', '103.84909538931907', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -60,6 +73,19 @@ CREATE TABLE `project` (
   `ketua_projek` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`kode_projek`, `nama_projek`, `domain`, `package`, `id_client`, `start_date`, `end_date`, `status`, `ketua_projek`) VALUES
+(1, 'Projek 1', 'kode.pcr.ac.id', 'Silver', 943752158, '2019-04-01', '2023-04-01', 'Berakhir', 'Doni Romdoni'),
+(2, 'Projek Luar Negeri 1', 'twintower.com', 'Gold', 943752159, '2020-04-01', '2022-08-01', 'Aktif', 'Doni Romdoni'),
+(3, 'Projek Sate', 'sate.com', 'Bronze', 943752156, '2019-04-01', '2021-04-01', 'Berakhir', 'Doni Romdoni'),
+(4, 'Projek PCR 2', 'projek.pcr.ac.id', 'Bronze', 943752158, '2017-04-01', '2019-04-01', 'Aktif', 'Doni Romdoni'),
+(5, 'Projek Inovindo', 'inovindo.com', 'Gold', 943752157, '2016-04-01', '2023-04-01', 'Aktif', 'Doni Romdoni'),
+(6, 'Projek Sate 2', 'pcr.co.id', 'Bronze', 943752158, '2019-04-01', '2021-04-01', 'Berakhir', 'Doni Romdoni'),
+(7, 'SISTIFO', 'nanyang.ac.sg', 'Gold', 943752160, '2019-01-01', '2022-01-01', 'Berakhir', 'Doni Romdoni');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +99,14 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `email`, `password`, `role`) VALUES
+(2, 'admin', 'admin@gmail.com', '$2y$10$O9LdsSkDqdT1MWQVsfF5h.aKX6nbK9R9hHxccj.rqDls1AvqEiZxO', 'Admin'),
+(3, 'tes', 'tes@gmail.com', '$2y$10$8KbPZ1fkGnDkkA8ox334meg1iKYUNbJKgDmEjJ.uzrnhLMgYuvAii', 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -105,19 +139,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=943752164;
 
 --
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `kode_projek` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `kode_projek` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
